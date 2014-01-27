04d179f8e81af73a9aee607f8cc8f29a038a9910
John Wiseman <jwiseman@morseproject.com>
2014-01-26 23:50:45 -0800

    Removed example HMM.

--- src/com/lemonodor/viterbi.clj
+++ src/com/lemonodor/viterbi.clj
@@ -112,21 +112,9 @@
           (recur (rest s) (first s))
           (recur (rest s) max))))))
 
+
 (defn pprint-hmm [hmm]
   (println "number of states: " (:n hmm) " number of observations:  " (:m hmm))
   (print "init probabilities: ") (pprint/pprint (:init-probs hmm))
   (print "emission probs: " ) (pprint/pprint (:emission-probs hmm))
   (print "state-transitions: " ) (pprint/pprint (:state-transitions hmm)))
-
-
-(def example-hmm
-  (make-hmm
-   :states [:healthy :fever]
-   :obs [:normal :cold :dizzy]
-   :start-p {:healthy 0.6, :fever 0.4}
-   :trans-p (let [t {:healthy {:healthy 0.7, :fever 0.3},
-                     :fever {:healthy 0.4, :fever 0.6}}]
-              #((t %1) %2))
-   :emit-p (let [t {:healthy {:normal 0.5, :cold 0.4, :dizzy 0.1},
-                    :fever {:normal 0.1, :cold 0.3, :dizzy 0.6}}]
-             #((t %1) %2))))
